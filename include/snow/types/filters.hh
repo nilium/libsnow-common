74deda4b27fa449e4df96fc48addf88526c2f582
Noel Cower <me@spifftastic.net>
2013-03-08 10:27:45 -0700

    Rename map_values to mapped_values. Add selected/rejected_values.

--- include/snow/types/filters.hh
+++ include/snow/types/filters.hh
@@ -4,11 +4,33 @@
 namespace snow {
 
 template <typename R, typename C, typename F, typename... ARGS>
-R map_values(const C &input, F &&func, ARGS&&... args)
+R mapped_values(const C &input, F &&func, ARGS&&... args)
 {
   R out;
   for (auto value : input)
     out.insert(out.end(), func(value, args...));
+  return out;
+}
+
+template <typename C, typename F, typename... ARGS>
+C selected_values(const C &input, F &&func, ARGS&&... args)
+{
+  C out;
+  for (auto value : input)
+    if (func(value, args...))
+      out.insert(out.end(), value);
+
+  return out;
+}
+
+template <typename C, typename F, typename... ARGS>
+C rejected_values(const C &input, F &&func, ARGS&&... args)
+{
+  C out;
+  for (auto value : input)
+    if (!func(value, args...))
+      out.insert(out.end(), value);
+
   return out;
 }
 
