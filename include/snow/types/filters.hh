dac88c0cc27307e4b6a0dfb25311cd52723198b2
Noel Cower <me@spifftastic.net>
2013-03-08 11:13:57 -0700

    Back to map_values.
    * map_values now takes an input and output argument. Both should be
      containers that work with the for-range loop and provide
      insert(iterator, value) and end() member functions.
    * select_values and reject_values added; both work similarly.
    * In all three cases, input and output parameters must be distinct
      references. Can't use 'restrict' for this, so it'll throw an exception
      if their addresses are the same.

--- include/snow/types/filters.hh
+++ include/snow/types/filters.hh
@@ -1,37 +1,67 @@
+// filters.hh -- Noel Cower -- Public Domain
+
 #ifndef __SNOW_COMMON__FILTERS_HH__
 #define __SNOW_COMMON__FILTERS_HH__
 
+#include <stdexcept>
+
 namespace snow {
 
-template <typename R, typename C, typename F, typename... ARGS>
-R mapped_values(const C &input, F &&func, ARGS&&... args)
+template <typename C, typename R, typename F, typename... ARGS>
+R &map_values(const C &input, R &output, F &&func, ARGS&&... args)
 {
-  R out;
+  if ((void *)&input == (void *)&output)
+    throw std::invalid_argument("input and output parameters must be distinct");
+
   for (auto value : input)
-    out.insert(out.end(), func(value, args...));
-  return out;
+    output.insert(output.end(), func(value, args...));
+  return output;
+}
+
+template <typename C, typename F, typename... ARGS>
+C &select_values(const C &input, C &output, F &&func, ARGS&&... args)
+{
+  if ((void *)&input == (void *)&output)
+    throw std::invalid_argument("input and output parameters must be distinct");
+
+  for (auto value : input)
+    if (func(value, args...))
+      output.insert(output.end(), value);
+
+  return output;
+}
+
+template <typename C, typename F, typename... ARGS>
+C &reject_values(const C &input, C &output, F &&func, ARGS&&... args)
+{
+  if ((void *)&input == (void *)&output)
+    throw std::invalid_argument("input and output parameters must be distinct");
+
+  for (auto value : input)
+    if (!func(value, args...))
+      output.insert(output.end(), value);
+
+  return output;
 }
 
 template <typename C, typename F, typename... ARGS>
 C selected_values(const C &input, F &&func, ARGS&&... args)
 {
-  C out;
+  C output;
   for (auto value : input)
     if (func(value, args...))
-      out.insert(out.end(), value);
-
-  return out;
+      output.insert(output.end(), value);
+  return output;
 }
 
 template <typename C, typename F, typename... ARGS>
 C rejected_values(const C &input, F &&func, ARGS&&... args)
 {
-  C out;
+  C output;
   for (auto value : input)
     if (!func(value, args...))
-      out.insert(out.end(), value);
-
-  return out;
+      output.insert(output.end(), value);
+  return output;
 }
 
 } // namespace snow
