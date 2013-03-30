72d9400f4c56cd4919d4e2e5c9db974f255e95ab
Noel Cower <me@spifftastic.net>
2013-03-30 14:59:52 -0700

    Add functions for iterating over objects in pools.

--- include/snow/types/object_pool.hh
+++ include/snow/types/object_pool.hh
@@ -3,7 +3,10 @@
 #define __SNOW_COMMON__OBJECT_POOL_HH__
 
 #include <new>
+#include <set>
 #include <vector>
+#include <functional>
+
 
 namespace snow {
 
@@ -20,10 +23,12 @@ template <typename T, typename IT = size_t,
           template <class CIT, class CIA = std::allocator<CIT>> class IC = std::vector>
 struct object_pool_t
 {
-  using object_t = T;
-  using index_t = IT;
-  using objects_t = OC<object_t>;
-  using indices_t = IC<index_t>;
+  using object_t            = T;
+  using index_t             = IT;
+  using objects_t           = OC<object_t>;
+  using indices_t           = IC<index_t>;
+  using object_iter_t       = std::function<void(object_t &, const index_t &)>;
+  using const_object_iter_t = std::function<void(const object_t &, const index_t &)>;
 
 
 
@@ -127,6 +132,36 @@ struct object_pool_t
 
   const object_t &operator [](const index_t index) const {
     return objects_[index];
+  }
+
+
+
+  void each_object(object_iter_t &iter)
+  {
+    std::set<index_t> index_set(unused_indices_.begin(), unused_indices_.end());
+    const auto index_term = index_set.end();
+    index_t index = 0;
+    const auto size = objects_.size();
+    for (; index < size; ++index) {
+      if (index_set.find(index) != index_term) {
+        iter(objects_[index], index);
+      }
+    }
+  }
+
+
+
+  void each_object_const(const const_object_iter_t &iter) const
+  {
+    std::set<index_t> index_set(unused_indices_.begin(), unused_indices_.end());
+    const auto index_term = index_set.end();
+    index_t index = 0;
+    const auto size = objects_.size();
+    for (; index < size; ++index) {
+      if (index_set.find(index) != index_term) {
+        iter(objects_[index], index);
+      }
+    }
   }
 
 
