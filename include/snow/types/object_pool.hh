039755a420fd76594568e84dcf5a2d938beca224
Noel Cower <me@spifftastic.net>
2013-04-06 02:08:43 -0600

    Add THREADSAFE argument to object_pool_t template.
    When set to true, uses std::mutex as its locking construct. Otherwise,
    it uses an internal dummylock_t that has no-ops for both lock() and
    unlock().

--- include/snow/types/object_pool.hh
+++ include/snow/types/object_pool.hh
@@ -6,6 +6,8 @@
 #include <set>
 #include <vector>
 #include <functional>
+#include <type_traits>
+#include <mutex>
 
 
 namespace snow {
@@ -18,7 +20,7 @@ namespace snow {
   IC, the index container, could be a set, deque, list, or vector. Both default
   to vector because there's not much point in changing either.
 */
-template <typename T, typename IT = size_t,
+template <typename T, typename IT = size_t, bool THREADSAFE = true,
           template <class COT, class COA = std::allocator<COT>> class OC = std::vector,
           template <class CIT, class CIA = std::allocator<CIT>> class IC = std::vector>
 struct object_pool_t
@@ -36,6 +38,7 @@ struct object_pool_t
   index_t reserve(ARGS&&... args)
   {
     index_t index = 0;
+    lock_.lock();
     if (unused_indices_.empty()) {
       index = objects_.size();
       objects_.emplace(objects_.end(), args...);
@@ -43,8 +46,9 @@ struct object_pool_t
       auto index_begin = unused_indices_.begin();
       index = *index_begin;
       unused_indices_.erase(index_begin);
-      new(&objects_[index]) object_t(args...);
+      new(&objects_.at(index)) object_t(args...);
     }
+    lock_.unlock();
     return index;
   }
 
@@ -55,6 +59,7 @@ struct object_pool_t
     static_assert(std::is_copy_constructible<object_t>::value,
                   "Cannot reserve with object - pool object type is not copy-constructible");
     index_t index = 0;
+    lock_.lock();
     if (unused_indices_.empty()) {
       index = objects_.size();
       objects_.emplace(objects_.end(), obj);
@@ -62,8 +67,9 @@ struct object_pool_t
       auto index_begin = unused_indices_.begin();
       index = *index_begin;
       unused_indices_.erase(index_begin);
-      new(&objects_[index]) object_t(obj);
+      new(&objects_.at(index)) object_t(obj);
     }
+    lock_.unlock();
     return index;
   }
 
@@ -74,6 +80,7 @@ struct object_pool_t
     static_assert(std::is_move_constructible<object_t>::value,
                   "Cannot reserve with rvalue - pool object type is not move-constructible");
     index_t index = 0;
+    lock_.lock();
     if (unused_indices_.empty()) {
       index = objects_.size();
       objects_.emplace(objects_.end(), obj);
@@ -81,8 +88,9 @@ struct object_pool_t
       auto index_begin = unused_indices_.begin();
       index = *index_begin;
       unused_indices_.erase(index_begin);
-      new(&objects_[index]) object_t(obj);
+      new(&objects_.at(index)) object_t(obj);
     }
+    lock_.unlock();
     return index;
   }
 
@@ -90,93 +98,127 @@ struct object_pool_t
 
   void collect(const index_t index)
   {
-    objects_[index].~object_t();
+    lock_.lock();
+    objects_.at(index).~object_t();
     unused_indices_.insert(unused_indices_.end(), index);
+    lock_.unlock();
   }
 
 
 
   std::pair<bool, index_t> index_of(const object_t &obj) const
   {
+    std::pair<bool, index_t> result { false, 0 };
+    lock_.lock();
     const object_t *addr = &obj;
     index_t index = 0;
     const auto length = objects_.size();
-    for (; index < length; ++index)
-      if (&objects_[index] == addr)
-        return {true, index};
-    return {false, 0};
+    for (; index < length; ++index) {
+      if (&objects_.at(index) == addr) {
+        result = { true, index };
+        break;
+      }
+    }
+    lock_.unlock();
+    return result;
   }
 
 
 
   object_t &at(const index_t index)
   {
-    return objects_[index];
+    lock_.lock();
+    object_t &ret = objects_.at(index);
+    lock_.unlock();
+    return ret;
   }
 
 
 
   const object_t &at(const index_t index) const
   {
-    return objects_[index];
+    lock_.lock();
+    object_t &ret = objects_.at(index);
+    lock_.unlock();
+    return ret;
   }
 
 
 
   object_t &operator [](const index_t index)
   {
-    return objects_[index];
+    lock_.lock();
+    object_t &ret = objects_.at(index);
+    lock_.unlock();
+    return ret;
   }
 
 
 
   const object_t &operator [](const index_t index) const {
-    return objects_[index];
+    lock_.lock();
+    object_t &ret = objects_.at(index);
+    lock_.unlock();
+    return ret;
   }
 
 
 
   void each_object(object_iter_t &iter)
   {
+    lock_.lock();
     std::set<index_t> index_set(unused_indices_.begin(), unused_indices_.end());
     const auto index_term = index_set.end();
     index_t index = 0;
     const auto size = objects_.size();
     for (; index < size; ++index) {
       if (index_set.find(index) != index_term) {
-        iter(objects_[index], index);
+        iter(objects_.at(index), index);
       }
     }
+    lock_.unlock();
   }
 
 
 
   void each_object_const(const const_object_iter_t &iter) const
   {
+    lock_.lock();
     std::set<index_t> index_set(unused_indices_.begin(), unused_indices_.end());
     const auto index_term = index_set.end();
     index_t index = 0;
     const auto size = objects_.size();
     for (; index < size; ++index) {
       if (index_set.find(index) != index_term) {
-        iter(objects_[index], index);
+        iter(objects_.at(index), index);
       }
     }
+    lock_.unlock();
   }
 
 
 
   void clear()
   {
+    lock_.lock();
     objects_.clear();
     unused_indices_.clear();
+    lock_.unlock();
   }
 
 
 
 private:
+  struct dummylock_t {
+    void lock() {}
+    void unlock() {}
+  };
+
+  using lock_t = typename std::conditional<THREADSAFE, std::mutex, dummylock_t>::type;
+
   objects_t   objects_;
   indices_t   unused_indices_;
+  mutable lock_t lock_;
 
 }; // struct object_pool_t
 
