ad747700cd8f51ad233e777e7631a0153a370e2b
Noel Cower <me@spifftastic.net>
2013-04-13 10:32:40 -0600

    Add s_throw, make exception-throwing optional
    If USE_EXCEPTIONS is defined, throws exceptions, if not, exit(1). This
    isn't included in the config.hh because it should be defined on a per-
    project basis, not something that affects everything. So, snow-common
    might not throw exceptions and just error out, but the project using
    it might still choose to throw exceptions.
    
    Also, all the ifdef __cplusplus crap is gone.

--- include/snow/logging/log.hh
+++ include/snow/logging/log.hh
@@ -3,38 +3,47 @@
 #ifndef __SNOW_COMMON__COMMON__LOG_HH__
 #define __SNOW_COMMON__COMMON__LOG_HH__
 
-#if defined(__cplusplus)
 #include <cstdio>
-#else
-#include <stdio.h>
-#endif
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
 
-#if S_PLATFORM_TOUCHPAD
-#include <PDL.h>
-#endif
 
 #ifndef S_EXPORT
 #define S_EXPORT __attribute__((visibility("default")))
 #endif
+
 
 #define SNOW_LOG_NOTHING          (0)
 #define SNOW_LOG_ERRORS           (1)
 #define SNOW_LOG_WARNINGS_ERRORS  (2)
 #define SNOW_LOG_EVERYTHING       (3)
 
+
 /*! Force the inclusion of logging macros regardless of release/debug mode. */
 #if !defined(FORCE_LOGGING)
 #define FORCE_LOGGING 0
 #endif
 
+
 #if !defined(SNOW_LOGGING_LEVEL)
 #define SNOW_LOGGING_LEVEL SNOW_LOG_EVERYTHING
 #endif
 
-#if defined(__cplusplus)
-extern "C"
+
+template <typename ...ARGS>
+void s_log_impl(const char *format, ARGS&&... args)
 {
-#endif
+  int length = snprintf(NULL, 0, format, std::forward<ARGS>(args)...) + 1;
+  std::vector<char> strbuf;
+  strbuf.resize(length);
+  snprintf(strbuf.data(), length, format, std::forward<ARGS>(args)...);
+  std::string str_temp(strbuf.data(), length - 2); // exclude \n
+  std::clog << str_temp << std::endl;
+}
+
 
 #if !defined(s_fatal_error)
 /*! Writes an error message to stderr then kills the program with the given
@@ -46,31 +55,43 @@ extern "C"
  *  @param[in] format The format for the error message.
  *  @param[in] error The error code to exit with.
  */
-#define USE_FATAL_ERROR_IMPL 1
-S_EXPORT void s_fatal_error_impl(const char *format, ...);
+template <typename EX_T, typename... ARGS>
+void s_fatal_error_impl(const char *format, ARGS&&... args)
+{
+  int length = snprintf(NULL, 0, format, std::forward<ARGS>(args)...) + 1;
+  std::vector<char> strbuf;
+  strbuf.resize(length);
+  snprintf(strbuf.data(), length, format, std::forward<ARGS>(args)...);
+  std::string str_temp(strbuf.data(), length - 2); // exclude \n
+  std::clog << str_temp << std::endl;
+#if USE_EXCEPTIONS
+  throw EX_T(str_temp);
+#else
+  exit(1);
+#endif
+} /* log_fatal */
 
+#define s_throw(EXCEPTION, FORMAT, args...) s_fatal_error_impl<EXCEPTION>("Fatal Error [%s:%s:%d]:\n    " FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
 /*! \brief Macro around ::log_fatal_ to pass in additional file, line number,
   and callee information to the format string.  This is never a no-op.
 */
-#define s_fatal_error(FORMAT, args...) s_fatal_error_impl("Fatal Error [%s:%s:%d]:\n    " FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
+#define s_fatal_error(FORMAT, args...) s_fatal_error_impl<std::runtime_error>("Fatal Error [%s:%s:%d]:\n    " FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
 #endif
 
-#if S_PLATFORM_TOUCHPAD
-#define s_log(STR, args...) PDL_Log((STR), ##args)
-#else
-#define s_log(STR, args...) fprintf(stderr, (STR), ##args)
-#endif
+
+#define s_log(STR, args...) s_log_impl((STR), ##args)
+
 
 /* if debugging or logging is forcibly enabled, then provide additional logging macros */
 #if !defined(NDEBUG) || FORCE_LOGGING
 #if !defined(s_log_error) && SNOW_LOGGING_LEVEL >= SNOW_LOG_ERRORS
-#define s_log_error(FORMAT, args...)   s_log("Error [%s:%s:%d]\n    "   FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
+#define s_log_error(FORMAT, args...)   s_log("Error [%s:%s:%d] "   FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
 #endif
 #if !defined(s_log_warning) && SNOW_LOGGING_LEVEL >= SNOW_LOG_WARNINGS_ERRORS
-#define s_log_warning(FORMAT, args...) s_log("Warning [%s:%s:%d]\n    " FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
+#define s_log_warning(FORMAT, args...) s_log("Warning [%s:%s:%d] " FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
 #endif
 #if !defined(s_log_note) && SNOW_LOGGING_LEVEL >= SNOW_LOG_EVERYTHING
-#define s_log_note(FORMAT, args...)    s_log("Note [%s:%s:%d]\n    "    FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
+#define s_log_note(FORMAT, args...)    s_log("Note [%s:%s:%d] "    FORMAT "\n", __FILE__, __FUNCTION__, __LINE__, ##args)
 #endif
 #endif
 
@@ -108,9 +129,5 @@ S_EXPORT void s_fatal_error_impl(const char *format, ...);
 
   In builds with NDEBUG defined, this will be a no-op.
 */
-
-#if defined(__cplusplus)
-}
-#endif
 
 #endif /* end of include guard: __SNOW_COMMON__COMMON__LOG_HH__ */
