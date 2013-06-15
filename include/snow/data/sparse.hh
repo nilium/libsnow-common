80dbb412149e2638530900a9f9663c08094da159
Noel Cower <ncower@gmail.com>
2013-06-15 02:46:19 -0700

    Doc comments for Sparse implementation.

--- include/snow/data/sparse.hh
+++ include/snow/data/sparse.hh
@@ -14,34 +14,73 @@
 namespace snow {
 
 
+/** @addtogroup Sparse Sparse
+  @{
+*/
+
+
+/** The Sparse parser namespace. */
 namespace sparse {
 
 
+/** Constants for node elements provided to a parse_func_t. */
 enum source_kind_t : int
 {
+  /** An error occurred reading the source. */
   SP_ERROR      = -1,
+  /** The parser encountered a node opening. */
   SP_OPEN_NODE  =  0,
+  /** The parser encountered a node closing. */
   SP_CLOSE_NODE =  1,
+  /** The parser encountered a name. */
   SP_NAME       =  2,
+  /** The parser encountered a value. Values necessarily follow names. */
   SP_VALUE      =  3,
+  /** The parser finished. */
   SP_DONE       =  4
 };
 
 
+/** Flags to indicate to a parser_t how to parse a document. */
 enum option_flags_t : int
 {
+  /**
+    Flag to tell the parser to consume excess whitespace in values. More than
+    one space in a row will be truncated to a single space.
+  */
   SP_CONSUME_WHITESPACE   = 0x1 << 0,
+  /**
+    Tells the parser to trim trailing spaces from values.
+  */
   SP_TRIM_TRAILING_SPACES = 0x1 << 1,
+  /**
+    Allows root nodes to be nameless.
+    @see SP_NAMELESS_NODES for more information.
+  */
   SP_NAMELESS_ROOT_NODES  = 0x1 << 2,
-  SP_NAMELESS_NODES       = 0x1 << 3,
+  /**
+    Allows nameless nodes anywhere in a Sparse document.
+
+    @note If this flag is set, before a nameless node is parsed, the parser
+    callback will receive a message for a name with an empty string.
+
+    This flag implies SP_NAMELESS_ROOT_NODES.
+  */
+  SP_NAMELESS_NODES       = 0x1 << 3 | SP_NAMELESS_ROOT_NODES,
+  /** Default parser options. Essentially an or-ing of all flags. */
   SP_DEFAULT_OPTIONS      = (SP_TRIM_TRAILING_SPACES |
                              SP_NAMELESS_NODES |
                              SP_CONSUME_WHITESPACE)
 };
 
 
+/**
+  A position in a Sparse document.
+*/
 struct S_EXPORT position_t {
+  /** The line in the document. */
   size_t line;
+  /** The column in the document. */
   size_t column;
 
   operator string() const;
@@ -51,22 +90,48 @@ struct S_EXPORT position_t {
 S_EXPORT std::ostream &operator << (std::ostream&, const position_t&);
 
 
-using parse_func_t = std::function<void(source_kind_t, const string&, position_t)>;
+/**
+  The parser function provided to a parser_t.
+  @param kind The kind of element the parser encountered.
+  @param str  The string for the encountered element. May be empty in the case
+              of unnamed nodes.
+  @param pos  Where the element was encountered.
+*/
+using parse_func_t = std::function<void(source_kind_t kind, const string &str, position_t pos)>;
 
 
+/** The Sparse parser class. */
 struct S_EXPORT parser_t
 {
+  /** Constructor. Takes parsing flags and a callback function. */
   parser_t(int options, parse_func_t callback);
+  /** Copy constructor. */
   parser_t(const parser_t &other); // Copies parser state, callback, and options
   virtual ~parser_t();
 
+  /**
+    Function to add source to the parser. The source will be parsed as it is
+    added.
+
+    @param source The source to parse.
+  */
   virtual void add_source(const string &source);
-  // Construction opens the parser. To finish parsing, close() must be called.
+  /**
+    Closes the parser, signalling that it end close any in-progress elements
+    and send the SP_DONE message to the callback.
+
+    If close() is not called, some data may not be fully parsed.
+  */
   virtual void close();
 
+  /** Returns whether the parser encountered an error. */
   inline virtual bool have_error() const { return !state_.error.empty(); }
+  /** Returns the error string for the parser. */
   inline virtual const string &error() const { return state_.error; }
-
+  /**
+    Returns whether the parser is still open. If true, add_source() may still
+    be called.
+  */
   inline virtual bool is_open() const { return !state_.closed; }
 
 
@@ -129,9 +194,14 @@ private:
 };
 
 
+/** @} */
+
+
 } // namespace sparse
 
 
 } // namespace snow
+
+
 
 #endif /* end __SNOW_COMMON__SPARSE_HH__ include guard */
