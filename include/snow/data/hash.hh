907d47dde0fa6921de8a4e2a7a812c7395a6c45f
Noel Cower <ncower@gmail.com>
2013-06-15 02:45:58 -0700

    Add doc comments for hash functions.

--- include/snow/data/hash.hh
+++ include/snow/data/hash.hh
@@ -7,28 +7,68 @@
 #include <cstdint>
 
 
+/**
+  @file
+  @ingroup HashFunctions
+*/
+
+
 namespace snow {
 
+/**
+  @addtogroup HashFunction Hash Functions
+  @{
+*/
 
-// Default seed is more or less random garbage that produces better results
-// than some other random garbage.
+
+/**
+  Default 32-bit seed for snow::hash32. It's more or less random garbage that
+  produces better results than some other random garbage.
+*/
 const uint32_t DEFAULT_HASH_SEED_32 = 0x9E2030F1U;
+/**
+  Default 64-bit seed for snow::hash64.
+*/
 const uint64_t DEFAULT_HASH_SEED_64 = 0x9E2030F19E2030F1ULL;
 
 
 
-// 32-bit
-// Wrapper around the other hash function.
-uint32_t hash32(const string &str, uint32_t seed = DEFAULT_HASH_SEED_32);
-uint32_t hash32(const char *str, const size_t length,
+/**
+  Produces a 32-bit hash of the input string.
+  @see snow::hash32(const char *, const size_t, uint32_t)
+*/
+S_EXPORT uint32_t hash32(const string &str, uint32_t seed = DEFAULT_HASH_SEED_32);
+
+/**
+  Produces a 32-bit hash of the input data.
+  @param str    The input data.
+  @param length The length of the input data.
+  @param seed   The seed for the input. If you want to concatenate string
+  hashes, you can use a previous string hash as the seed and it will be the
+  same as hashing the combined strings.
+*/
+S_EXPORT uint32_t hash32(const char *str, const size_t length,
                 uint32_t seed = DEFAULT_HASH_SEED_32);
 
+/**
+  Produces a 64-bit hash of the input string.
+  @see snow::hash64(const char *, const size_t, uint64_t)
+*/
+S_EXPORT uint64_t hash64(const string &str, uint64_t seed = DEFAULT_HASH_SEED_64);
 
-
-// 64-bit
-uint64_t hash64(const string &str, uint64_t seed = DEFAULT_HASH_SEED_64);
-uint64_t hash64(const char *str, const size_t length,
+/**
+  Produces a 64-bit hash of the input data.
+  @param str    The input data.
+  @param length The length of the input data.
+  @param seed   The seed for the input. If you want to concatenate string
+  hashes, you can use a previous string hash as the seed and it will be the
+  same as hashing the combined strings.
+*/
+S_EXPORT uint64_t hash64(const char *str, const size_t length,
                 uint64_t seed = DEFAULT_HASH_SEED_64);
+
+
+/** @} */
 
 
 } // namespace snow
--- include/snow/data/sha256.hh
+++ include/snow/data/sha256.hh
@@ -12,15 +12,39 @@
 namespace snow {
 
 
+/**
+  @addtogroup HashFunction
+  @{
+*/
+
+/**
+  Function to get the SHA256 of an arbitrary chunk of data. Requires OpenSSL to
+  be linked in application code if used.
+  @param data The data to hash.
+  @param length The length of the data to hash.
+  @param hash An output buffer at least SHA256_DIGEST_LENGTH bytes in length to
+  store the resulting hash.
+*/
 S_EXPORT void sha256(const char *data, size_t length, char hash[SHA256_DIGEST_LENGTH]);
 
 
 
+/**
+  Function to get the SHA256 of an arbitrary chunk of data. T must provide a
+  T::value_type typedef, a `data()` member function that returns a pointer, and
+  a `size()` member function to return the number of elements in the data.
+  @param data The data to hash.
+  @param hash An output buffer at least SHA256_DIGEST_LENGTH bytes in length to
+  store the resulting hash.
+*/
 template <typename T>
-inline void sha256(const std::basic_string<T> &string, char hash[65])
+inline void sha256(const T &data, char hash[SHA256_DIGEST_LENGTH])
 {
-  sha256((const char *)string.c_str(), string.size() * sizeof(T));
+  sha256((const char *)data.data(), data.size() * sizeof(T::value_type));
 }
+
+
+/** @} */
 
 
 } // namespace snow
