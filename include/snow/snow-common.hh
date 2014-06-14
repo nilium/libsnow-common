fcd174d81869324566d7f6d4ae3cc6546f2bc269
Noel Cower <ncower@gmail.com>
2014-06-13 18:06:25 -0700

    Move snow-common.hh into snow/ subdirectory.

---
+++ include/snow/snow-common.hh
@@ -0,0 +1,46 @@
+/*
+ * Copyright Noel Cower 2013 - 2014.
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+
+#ifndef __SNOW_COMMON__SNOW_COMMON_HH__
+#define __SNOW_COMMON__SNOW_COMMON_HH__
+
+#include "config.hh"
+
+#include "thread.hh"
+
+// Math
+#include "math/math.hh"
+#ifndef S_NO_INCLUDE_MATH3D
+// Because these headers are a bit large-ish, optionally exclude them.
+#include "math/math3d.hh"
+#endif
+
+// Types
+#include "types/binpack.hh"
+#include "types/object_pool.hh"
+#include "types/range.hh"
+#include "types/range_set.hh"
+#include "types/slot_image.hh"
+#include "types/slot_mask.hh"
+#include "types/triple_buffer.hh"
+#include "types/types_2d.hh"
+
+// Data
+#include "data/hash.hh"
+#include "data/sparse.hh"
+
+// Strings
+#include "string/string.hh"
+#include "string/compare.hh"
+#include "string/split.hh"
+
+// Memory
+#include "memory/ref_counter.hh"
+
+#endif /* end __SNOW_COMMON__SNOW_COMMON_HH__ include guard */
