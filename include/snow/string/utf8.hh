f39a8782b79783950f4af94c2d3925e258327fd1
Noel Cower <ncower@gmail.com>
2014-06-14 15:55:08 -0700

    Add utf8.hh/cc from Scolex.
    Spent enough time fiddling with it that I'm happy.

---
+++ include/snow/string/utf8.hh
@@ -0,0 +1,286 @@
+/*
+ * Copyright Noel Cower 2014.
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef __SNOW_COMMON__UTF8_HH__
+#define __SNOW_COMMON__UTF8_HH__
+
+
+#include <cstdint>
+
+
+namespace snow
+{
+
+
+namespace utf8
+{
+
+
+enum : uint32_t
+{
+  /* Returned by utf8::peek_code and utf8::next_code if either fails. */
+  UTF8_INVALID_CODE            = 0xFFFFFFFFu,
+
+  /* Alternatives you might use in peek/next for invalid codes */
+  UTF8_REPLACEMENT_CHAR        = 0xFFFDu,
+  UTF8_NONCHARACTER1           = 0xFFFEu,
+  UTF8_NONCHARACTER2           = 0xFFFFu,
+  UTF8_OBJECT_REPLACEMENT_CHAR = 0xFFFCu,
+};
+
+
+int const UTF8_BITS_INTERMEDIATE = 6;
+
+
+enum : uint32_t {
+  UTF8_MASK_INTERMEDIATE      = 0xC0u,
+  UTF8_NAME_INTERMEDIATE      = (UTF8_MASK_INTERMEDIATE << 1) & 0xFF,
+  UTF8_VAL_MASK_INTERMEDIATE  = (~UTF8_MASK_INTERMEDIATE) & 0xFF,
+
+  UTF8_MASK_0     = 0x80u,
+  UTF8_NAME_0     = (UTF8_MASK_0 << 1) & 0xFF,
+  UTF8_VAL_MASK_0 = (~UTF8_MASK_0) & 0xFFu,
+
+  UTF8_MASK_1     = 0xE0u,
+  UTF8_NAME_1     = (UTF8_MASK_1 << 1) & 0xFF,
+  UTF8_VAL_MASK_1 = (~UTF8_MASK_1) & 0xFFu,
+
+  UTF8_MASK_2     = 0xF0u,
+  UTF8_NAME_2     = (UTF8_MASK_2 << 1) & 0xFF,
+  UTF8_VAL_MASK_2 = (~UTF8_MASK_2) & 0xFFu,
+
+  UTF8_MASK_3     = 0xF8u,
+  UTF8_NAME_3     = (UTF8_MASK_3 << 1) & 0xFF,
+  UTF8_VAL_MASK_3 = (~UTF8_MASK_3) & 0xFFu,
+};
+
+
+/*==============================================================================
+  utf8::value_mask__
+
+    Returns the value mask for a UTF-8 bitmask.
+==============================================================================*/
+constexpr uint32_t value_mask__(uint32_t mask)
+{
+  return (~mask) & 0xFFu;
+}
+
+
+/*==============================================================================
+  utf8::mask_name__
+
+    Returns the name for a UTF-8 bitmask.
+==============================================================================*/
+constexpr uint32_t mask_name__(uint32_t mask)
+{
+  return (mask << 1) & 0xFFu;
+}
+
+
+/*==============================================================================
+  utf8::intermediate_byte__
+
+    Returns the intermediate byte for the given code and fourth of the
+    first 24 bits of the code.
+==============================================================================*/
+constexpr uint32_t intermediate_byte__(uint32_t code, int fourth)
+{
+  return {
+    ((code >> (fourth * UTF8_BITS_INTERMEDIATE)) &
+      UTF8_VAL_MASK_INTERMEDIATE) |
+    UTF8_NAME_INTERMEDIATE
+  };
+}
+
+
+/*==============================================================================
+  utf8::octets_for_code
+
+    Returns the number of octets required for the given code, or zero if the
+    code can't be represented as a well-formed UTF-8 byte sequence.
+==============================================================================*/
+int octets_for_code(uint32_t code);
+
+
+/*==============================================================================
+  utf8::next_octet__
+
+    Gets the next octet from the iterator as a uint32_t. Does not check whether
+    the iterator is valid. Internal use only.
+==============================================================================*/
+template <class IT>
+uint32_t next_octet__(IT const &start)
+{
+  return static_cast<uint32_t>(*start) & 0xFF;
+}
+
+
+/*==============================================================================
+  utf8::read_BOM
+
+    Reads the byte-order marker of octets 0xEF 0xBB 0xBF and returns true if
+    the BOM was recognized, otherwise false. The iterator is advanced past the
+    BOM only if it's present, otherwise it remains unchanged.
+==============================================================================*/
+template <class IT>
+bool read_BOM(IT &start, IT const &end)
+{
+  static uint32_t const BOM[3] { 0xEF, 0xBB, 0xBF };
+
+  if (start == end) {
+    return false;
+  }
+
+  IT iter { start };
+  int index = 0;
+
+  for (; index < 3 && iter != end; ++iter, ++index) {
+    uint32_t byte = next_octet__(iter);
+    if (byte != BOM[index]) {
+      return false;
+    }
+  }
+
+  start = iter;
+
+  return true;
+}
+
+
+/*==============================================================================
+  utf8::next_code
+
+    Reads a code given a character iterator -- the characters do not necessarily
+    have to be uint8_t though it helps since they're going to get cast to
+    unsigned and truncated otherwise. If the code is invalid, the iterator is
+    advanced to the next usable byte, then returns UTF8_INVALID_CODE.
+==============================================================================*/
+template <class IT>
+uint32_t next_code(IT &iter, IT const &end, uint32_t invalid = UTF8_INVALID_CODE)
+{
+  static uint32_t const markers[4] = {
+    UTF8_MASK_0, UTF8_MASK_1, UTF8_MASK_2, UTF8_MASK_3,
+  };
+
+  if (iter == end) {
+    return invalid;
+  }
+
+  uint32_t code = next_octet__(iter);
+  int count = 0;
+
+  for (; count < 4; ++count) {
+    uint32_t const mask = markers[count];
+    uint32_t const name = (mask << 1) & 0xFF;
+    uint32_t const vmask = (~mask) & 0xFF;
+
+    if ((code & mask) != name) {
+      // Invalid initial octet, skip all intermediate octets until a valid one is
+      // found then return the requested code for invalids.
+      for (; iter != end; ++iter) {
+        code = next_octet__(iter);
+        if ((code & UTF8_MASK_INTERMEDIATE) != UTF8_NAME_INTERMEDIATE) {
+          break;
+        }
+      }
+
+      return invalid;
+    }
+
+    code &= vmask;
+  }
+
+  ++iter;
+
+  for (; count; ++iter, --count) {
+    if (iter == end) {
+      return invalid;
+    }
+
+    uint32_t const next_code = next_octet__(iter);
+
+    if ((next_code & UTF8_MASK_INTERMEDIATE) != UTF8_NAME_INTERMEDIATE) {
+      return invalid;
+    }
+
+    code = (code << UTF8_BITS_INTERMEDIATE) | (next_code & UTF8_VAL_MASK_INTERMEDIATE);
+  }
+
+  if (octets_for_code(code) == 0) {
+    return invalid;
+  }
+
+  return code;
+}
+
+
+/*==============================================================================
+  utf8::peek_code
+
+    Peeks the next code from the given iterator and returns it if it's valid,
+    otherwise returns UTF8_INVALID_CODE. Does not advance the iterator.
+==============================================================================*/
+template <class IT>
+uint32_t peek_code(IT const &iter, IT const &end, uint32_t invalid = UTF8_INVALID_CODE)
+{
+  IT dry { iter };
+  return next_code<IT, invalid>(dry, end, invalid);
+}
+
+
+/*==============================================================================
+  utf8::next_is_valid
+
+    Returns true if the result of peek_code is valid, otherwise false.
+==============================================================================*/
+template <class IT>
+bool next_is_valid(IT const &iter, IT const &end)
+{
+  return peek_code(iter, end, UTF8_INVALID_CODE) != UTF8_INVALID_CODE;
+}
+
+
+/*==============================================================================
+  utf::put_code
+
+    Writes a given code to the iterator as a sequence of UTF-8 octets.
+    Returns 0 if the code is not representable as a well-formed UTF-8 octet
+    sequence. The output iterator is assumed to take octets of some kind.
+==============================================================================*/
+template <class IT>
+int put_code(IT &iter, uint32_t code)
+{
+  static constexpr uint32_t masks[4] {
+    UTF8_MASK_0, UTF8_MASK_1, UTF8_MASK_2, UTF8_MASK_3
+  };
+
+  int const num_octets = octets_for_code(code);
+  if (num_octets == 0) {
+    return 0;
+  }
+
+  int const intermediate_bytes = num_octets - 1;
+  uint32_t const mask = masks[intermediate_bytes];
+
+  *(iter++) = mask_name__(mask) |
+    (value_mask__(mask) & (code >> (UTF8_BITS_INTERMEDIATE * intermediate_bytes)));
+
+  for (int rep = 1; rep < num_octets; ++rep, ++iter) {
+    *iter = intermediate_byte__(code, intermediate_bytes - rep);
+  }
+
+  return num_octets;
+}
+
+
+} // namespace utf8
+
+
+} // namespace snow
+
+#endif /* end __SCOLEX_UTF8_HH__ include guard */
---
+++ src/string/utf8.cc
@@ -0,0 +1,49 @@
+// Copyright Noel Cower 2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <snow/config.hh>
+#include <snow/string/utf8.hh>
+
+
+namespace snow
+{
+
+
+namespace utf8
+{
+
+
+int octets_for_code(uint32_t code)
+{
+  struct {
+    uint32_t const begin;
+    uint32_t const end;
+    int const octets;
+  } const unicode_ranges[] = {
+    { 0x000000u, 0x00007Fu, 1 },
+    { 0x000080u, 0x0007FFu, 2 },
+    { 0x000800u, 0x000FFFu, 3 },
+    { 0x001000u, 0x00CFFFu, 3 },
+    { 0x00D000u, 0x00D7FFu, 3 },
+    { 0x00E000u, 0x00FFFFu, 3 },
+    { 0x010000u, 0x03FFFFu, 4 },
+    { 0x040000u, 0x0FFFFFu, 4 },
+    { 0x100000u, 0x10FFFFu, 4 }
+  };
+
+  for (auto const &range : unicode_ranges) {
+    if (range.begin <= code && code <= range.end) {
+      return range.octets;
+    }
+  }
+
+  return 0;
+}
+
+
+} // namespace utf8
+
+
+} // namespace snow
