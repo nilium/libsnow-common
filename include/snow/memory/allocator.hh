7d1be61a147abc86dccfaf9ebe71db5865bbb8c8
Noel Cower <ncower@gmail.com>
2014-06-28 17:58:30 -0700

    allocator: Add allocator.hh
    Covers basic allocators right now:
    
    - mallocator and aligned_mallocator both return memory allocated by
      malloc. It's safe to release memory allocated by one mallocator using
      another mallocator of the same type (meaning an
      aligned_mallocator<256> can release any memory allocated by other
      aligned_mallocator<256> instances, but not <16> instances).
    
    - deallocators are just ways of passing a deallocator to an
      std::unique_ptr and such for trivially destructible types.
      simple_deallocator just creates an instance of the allocator type
      and deallocates the block while bound_deallocator requires a
      reference to an allocator to use for deallocation.
    
    - aligned_mallocator, for alignments 1, 2, 4, 8, and 16 do nothing
      special and act as a wrapper around mallocator.

---
+++ include/snow/memory/allocator.hh
@@ -0,0 +1,177 @@
+/*
+ * Copyright Noel Cower 2014.
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+
+#ifndef __SNOW_COMMON__ALLOCATOR_HH__
+#define __SNOW_COMMON__ALLOCATOR_HH__
+
+
+#include <cstdlib>
+#include <functional>
+#include "align.hh"
+
+
+namespace snow
+{
+
+
+/*
+
+struct allocator
+{
+  // May be a struct or typedef, but must declare a deallocator function type.
+  // The deallocator need only implement an operator () (void *). It may be
+  // noexcept.
+  typedef deallocator_type ...;
+
+  // Must return a deallocator function type.
+  deallocator_type deallocator();
+
+  // Either a copy-constructor or a move-constructor must be supported by an
+  // allocator.
+  allocator(allocator const &) = required unless move is supported;
+  allocator(allocator &&) = required unless copy is supported;
+
+  void *allocate(size_t bytes);
+  void deallocate(void *ptr) noexcept;
+};
+
+*/
+
+
+
+template <typename Allocator>
+struct simple_deallocator
+{
+  void operator () (void *ptr) noexcept
+  {
+    Allocator{}.deallocate(ptr);
+  }
+};
+
+
+
+template <typename Allocator>
+struct bound_deallocator
+{
+  Allocator &_alloc;
+
+  void operator () (void *ptr) noexcept
+  {
+    _alloc.deallocate(ptr);
+  }
+};
+
+
+
+struct mallocator
+{
+  void *allocate(size_t bytes) { return std::malloc(bytes); }
+  void deallocate(void *ptr) noexcept { std::free(ptr); }
+
+  using deallocator_type = simple_deallocator<mallocator>;
+
+  deallocator_type deallocator()
+  {
+    return deallocator_type {};
+  }
+};
+
+
+
+template <size_t Align = 16>
+struct aligned_mallocator
+{
+  static size_t const alignment = Align;
+
+  void *allocate(size_t bytes);
+  void deallocate(void *ptr) noexcept;
+
+  using deallocator_type = simple_deallocator<aligned_mallocator>;
+
+  deallocator_type deallocator()
+  {
+    return deallocator_type {};
+  }
+};
+
+
+
+template <size_t Align>
+void aligned_mallocator<Align>::deallocate(void *p) noexcept
+{
+  void *const actual = ((void **)p)[-1];
+  std::free(actual);
+}
+
+
+
+template <size_t Align>
+void *aligned_mallocator<Align>::allocate(size_t const size_bytes)
+{
+  size_t const size_with_ptr = sizeof(void *) + size_bytes;
+  size_t const aligned_size  = align(size_with_ptr, Align);
+
+  void *const  block   = std::malloc(aligned_size);
+  void **const wrapper = (void **)align(uintptr_t(block) + sizeof(void *), Align);
+
+  wrapper[-1] = block;
+
+  return wrapper;
+}
+
+
+
+template <>
+struct aligned_mallocator<1> : public mallocator
+{
+  using mallocator::mallocator;
+  static size_t const alignment = Align;
+};
+
+
+
+template <>
+struct aligned_mallocator<2> : public mallocator
+{
+  using mallocator::mallocator;
+  static size_t const alignment = Align;
+};
+
+
+
+template <>
+struct aligned_mallocator<4> : public mallocator
+{
+  using mallocator::mallocator;
+  static size_t const alignment = Align;
+};
+
+
+
+template <>
+struct aligned_mallocator<8> : public mallocator
+{
+  using mallocator::mallocator;
+  static size_t const alignment = Align;
+};
+
+
+
+template <>
+struct aligned_mallocator<16> : public mallocator
+{
+  using mallocator::mallocator;
+  static size_t const alignment = Align;
+};
+
+
+} // namespace snow
+
+
+#endif /* end __ALLOCATOR_HH__ include guard */
