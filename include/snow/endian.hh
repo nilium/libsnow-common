a986143e8f250ce8391ef7c7cb706bcfcc05bf52
Noel Cower <ncower@gmail.com>
2014-07-09 23:41:34 -0700

    Add endian.hh
    Just defines a few things. Taken mostly from scolex.

---
+++ include/snow/endian.hh
@@ -0,0 +1,52 @@
+/*
+ * Copyright Noel Cower 2013.
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+
+#ifndef __SNOW_COMMON__ENDIAN_HH__
+#define __SNOW_COMMON__ENDIAN_HH__
+
+#include <snow/build-config.hh>
+
+
+#ifndef S_NETWORK_IS_BIG_ENDIAN
+#define S_NETWORK_IS_BIG_ENDIAN 0
+#endif
+
+
+#ifndef S_HOST_IS_BIG_ENDIAN
+#define S_HOST_IS_BIG_ENDIAN 0
+#endif
+
+
+namespace snow
+{
+
+
+enum class endian_t
+{
+  little  = 1,
+  big     = 2,
+  // Mixed endianness unsupported
+
+  #if S_HOST_IS_BIG_ENDIAN
+  host = big,
+  #else
+  host = little,
+  #endif
+
+  #if S_NETWORK_IS_BIG_ENDIAN
+  network = big,
+  #else
+  network = little,
+  #endif
+};
+
+
+} // namespace snow
+
+#endif /* end __SNOW_COMMON__ENDIAN_HH__ include guard */
