31d0b3d038560f086bf73808e0203fbe4da323d8
Noel Cower <ncower@gmail.com>
2014-07-12 23:21:27 -0700

    option: Use doxygen doc comments.
    Since nothing currently supports the other odd doc comment style I use.
    
    I'd consider fixing that, but parsing C++ is a nightmare and a half.

--- include/snow/types/option.hh
+++ include/snow/types/option.hh
@@ -62,14 +62,24 @@ template <typename T>
 class option_t;
 
 
+/**
+  Struct contianing type aliases for an option's value_type and self type.
+*/
 template <typename T>
 struct optional_bits
 {
+  /// An option of type T's value_type. An alias for the decayed form of T.
   using value_type = typename std::decay<T>::type;
+  /// An option of type T's self type, otherwise an option of type
+  /// option_t<value_type>.
   using type = option_t<value_type>;
 };
 
 
+/**
+  Type alias using the decayed form of T for an option. Ensures that you don't
+  try to create an option of a reference, volatile, etc. type.
+*/
 template <typename T>
 using optional = typename optional_bits<T>::type;
 
@@ -81,27 +91,35 @@ template <typename T, typename... ARGS>
 optional<T> make_optional(ARGS&&... args);
 
 
-/*==============================================================================
 
-  Option -- may be either defined (has some value) or undefined, in which case
-  it has no value.
+/**
+  @brief Optional value type. Holds a possibly-defined value.
 
-==============================================================================*/
+  Options may be either defined (has some value) or undefined, in which case
+  it has no value. All options hold storage for their own value.
+*/
 template <typename T>
 class option_t final
 {
 public:
   // Occasionally useful if you need the value type of an option, but this is
   // mainly here so flat_map can infer the value type of its returned option_t.
+  // Recommended that you use the optional<T> type alias to avoid issues with
+  // incompatible types despite using the same value_type.
+
+  /// @brief The bits structure used to determine this option's value_type.
   using bits = optional_bits<T>;
+  /// @brief The value_type of the option, obtained from bits::value_type.
   using value_type = typename bits::value_type;
 
 private:
-  // Bit of a hack to keep the private emplace-like constructor. By passing a
-  // ctor_bit_t as the constructor's first argument, this basically ensures it
-  // knows which ctor to use.
+  /// Bit of a hack to keep the private emplace-like constructor. By passing a
+  /// ctor_bit_t as the constructor's first argument, this basically ensures it
+  /// knows which ctor to use.
   enum ctor_bit_t
   {
+    /// CTOR enum argument used to disambiguate which constructor to use when
+    /// constructing optional values in place.
     CTOR
   };
 
@@ -113,19 +131,29 @@ private:
 
   using store_t = typename std::aligned_storage<SIZE, ALIGNMENT>::type;
 
+  /// Whether the option has a defined value (true if so, otherwise false).
   bool _defined;
+  /// Storage for any value to be held by the option. Is aligned according to
+  /// the value provided by std::alignment_of.
   store_t _data;
 
+  /// Returns a value_type pointer to the data used by the option. May have
+  /// an undefined value, so dereferencing it isn't guaranteed to be safe
+  /// unless _defined is true.
   value_type *typed()
   {
     return reinterpret_cast<value_type *>(&_data);
   }
 
+  /// Returns a value_type pointer to the data used by the option. May have
+  /// an undefined value, so dereferencing it isn't guaranteed to be safe
+  /// unless _defined is true.
   value_type const *typed() const
   {
     return reinterpret_cast<value_type const *>(&_data);
   }
 
+  /// Throws an std::runtime_error if the option is undefined.
   void throw_if_undefined() const
   {
     if (!_defined) {
@@ -133,80 +161,118 @@ private:
     }
   }
 
+  /// Destroys any value held by the option and makes it undefined.
   void dispose();
 
   friend class option_t<std::nullptr_t>;
 
+  /**
+    In-place constructor for options with defined values. Must pass CTOR
+    enum value as the first argument in order for the compiler to properly
+    disambiguate the constructor.
+  */
   template <typename... ARGS>
   option_t(ctor_bit_t, ARGS&&... args);
 
 public:
 
-  /*============================================================================
-    make(args...)
+  /**
+    @brief Constructs a new instance of an option with a defined value in-place.
 
-      Returns an option with a defined value constructed in-place rather than by
-      copying or moving an existing defined (or undefined) option). Passes any
-      arguments received to one of the optional value type's constructors,
-      assuming such a constructor exists.
+    Returns an option with a defined value constructed in-place rather than by
+    copying or moving an existing defined (or undefined) option). Passes any
+    arguments received to one of the optional value type's constructors,
+    assuming such a constructor exists.
 
-      The reason for make vs. just providing a forwarding ctor is that the
-      forwarding ctor could be option_t's ctor, in which case multiple
-      definitions of an option_t ctor are created. So, this keeps things
-      sort of normal.
-  ============================================================================*/
+    The reason for make vs. just providing a forwarding ctor is that the
+    forwarding ctor could be option_t's ctor, in which case multiple
+    definitions of an option_t ctor are created. So, this keeps things
+    sort of normal.
+  */
   template <typename... ARGS>
   static option_t make(ARGS&&... args);
 
+  /**
+    @brief Default constructor.
+
+    Constructs an empty optional.
+  */
   option_t() noexcept;
   ~option_t();
 
+  /// @brief Constructs an empty optional by copying nothing.
   option_t(none_t const &) noexcept; // same as default ctor
+  /// @brief Copy constructor.
   option_t(option_t const &copied);
+  /// @brief Move constructor.
   option_t(option_t &&moved);
 
+  /// @brief None-assignment operator. Assigns nothing, making the option
+  /// undefined.
   option_t &operator = (none_t const &);
+  /// @brief Copy-assignment operator.
   option_t &operator = (option_t const &);
+  /// @brief Move-assignment operator.
   option_t &operator = (option_t &&);
 
+  /// @brief None-assignment. Assigns nothing, making the option undefined.
   option_t &assign(none_t const &);
+  /// @brief Copy-assignment.
   option_t &assign(option_t const &);
+  /// @brief Move-assignment.
   option_t &assign(option_t &&);
 
+  /// @brief Returns whether the option is defined.
   bool is_defined() const noexcept { return _defined; }
+  /// @brief Returns whether the option is empty (undefined). Opposite of
+  /// is_defined.
   bool is_empty() const noexcept { return !is_defined(); }
 
+  /// @brief Gets the value of the option if defined, otherwise throws an
+  /// std::runtime_error exception.
   value_type &get()
   {
     throw_if_undefined();
     return *typed();
   }
 
+  /// @brief Gets the value of the option if defined, otherwise throws an
+  /// std::runtime_error exception.
   value_type const &get() const
   {
     throw_if_undefined();
     return *typed();
   }
 
+  /// @brief Dereference operator. Gets the value of the option if defined,
+  /// otherwise throws an std::runtime_error exception.
   value_type &operator * () { return get(); }
+  /// @brief Dereference operator. Gets the value of the option if defined,
+  /// otherwise throws an std::runtime_error exception.
   value_type const &operator * () const { return get(); }
 
+  /// @brief Dereference operator -- returns a pointer to the value of the
+  /// option.
   value_type *operator -> () const { return get(); }
 
+  /// @brief Implicit bool conversion -- returns whether the option is defined.
   operator bool () const { return is_defined(); }
+  /// @brief Unary logical not operator -- returns whether the option is empty.
   bool operator ! () const { return is_empty(); }
 
+  /// @brief Returns an empty (default constructed) option_t.
   static auto empty() noexcept -> option_t;
 
 
-  /*============================================================================
-    operator () (application_fn) -- call / application operator
+  /**
+   @brief Apply function -- calls function f, passing the value of the option
+   to f, if and only if the option has a defined value. Receives a const
+   reference to or copy of the option's value.
 
-      Given an application function that takes a reference of the type held by
-      the underlying option, the call operator will only call the given function
-      if the value of the option is defined. If empty, it returns early.
-  ============================================================================*/
-  // Apply functions
+   Given an application function that takes a reference of the type held by
+   the underlying option, the call operator will only call the given function
+   if the value of the option is defined. If empty, it returns early.
+  */
   template <typename F>
   void operator () (F &&f) const
   {
@@ -216,6 +282,15 @@ public:
   }
 
 
+  /**
+   @brief Apply function -- calls function f, passing the value of the option
+   to f, if and only if the option has a defined value. Receives a reference to
+   or copy of the option's value.
+
+   Given an application function that takes a reference of the type held by
+   the underlying option, the call operator will only call the given function
+   if the value of the option is defined. If empty, it returns early.
+  */
   template <typename F>
   void operator () (F &&f)
   {
@@ -225,30 +300,30 @@ public:
   }
 
 
-  /*============================================================================
-    map
+  /**
+    @brief Maps the value of this option to another value, returning a new
+    option with the result of function f.
 
-      This is a simple map function -- given a function that takes T and returns
-      type U (may be anything), it returns an option_t<U> with a defined value
-      if the option is defined, otherwise returns an empty option. Contrast this
-      with flat_map's behavior, which is the same, but uses a function that
-      returns an option_t<U> instead of just U. If your map function may map
-      from a defined value to an empty value, then use flat_map. If your map
-      function always has a defined value, use map.
-  ============================================================================*/
-
+    This is a simple map function -- given a function that takes T and returns
+    type U (may be anything), it returns an optional<U> with a defined value
+    if the option is defined, otherwise returns an empty option. Contrast this
+    with flat_map's behavior, which is the same, but uses a function that
+    returns an optional<U> instead of just U. If your map function may map
+    from a defined value to an empty value, then use flat_map. If your map
+    function always has a defined value, use map.
+  */
   template <typename F>
   auto map(F &&f) const -> optional<decltype(f(get()))>;
 
 
-  /*============================================================================
-    flat_map
+  /**
+    @brief Maps the value of this option to another value, returning the result
+    of function f. Function f must return an optional type.
 
-      Similar to map, but the map function must return an option_t<U> instead of
-      just a U value. This allows it to map any defined value to an empty value.
-      If your function always returns a defined value, use map.
-  ============================================================================*/
-  // map T -> option_t<U>
+    Similar to map, but the map function must return an optional<U> instead of
+    just a U value. This allows it to map any defined value to an empty value.
+    If your function always returns a defined value, use map.
+  */
   template <typename F>
   auto flat_map(F &&f) const -> optional<typename decltype(f(get()))::value_type>;
 };
@@ -493,13 +568,14 @@ std::ostream &operator << (std::ostream &out, option_t<T> const &opt)
 
 
 
-/*==============================================================================
+/**
+  @brief Class representing an empty type -- i.e., a unit value, or nothing.
+  May be implicitly cast to an empty optional<T> of any kind.
 
   none_t is an empty type that has no value and is mostly only ever used when
   assigning the constexpr value none to options. It really has no purpose
   outside of that use.
-
-==============================================================================*/
+*/
 struct none_t final
 {
   struct value_type final {};
@@ -520,6 +596,9 @@ struct none_t final
 
 
 
+/// @brief Global constant none -- may be used to assign no value to an optional
+/// or otherwise passed around to indicate no value, since it can be implicitly
+/// converted to an optional of any type.
 constexpr none_t none {};
 
 
@@ -538,15 +617,15 @@ inline std::ostream &operator << (std::ostream &out, none_t::value_type const &)
 
 
 
-/*==============================================================================
+/**
+  @brief nullptr_t specialization of option_t.
 
-  nullptr_t specialization of option_t -- generally a bad idea to use this, but
-  the specialization's needed to avoid attempts to do horrible things like
-  `new std::nullptr_t` which as we all know is bad. Implements a compatible
-  subset of option_t's interface, though less, as this shouldn't be used except
-  to do things like assign some(nullptr) to an option_t<T != nullptr_t>.
-
-==============================================================================*/
+  Generally a bad idea to use this, but the specialization's needed to avoid
+  attempts to do horrible things like `new std::nullptr_t` which as we all know
+  is bad. Implements a compatible subset of option_t's interface, though less,
+  as this shouldn't be used except to do things like assign some(nullptr) to
+  an option_t<T != nullptr_t>.
+*/
 template <>
 class option_t<std::nullptr_t> final
 {
@@ -566,15 +645,22 @@ public:
 
 
 
+  /**
+    @brief Default constructor.
+
+    Constructs an empty optional of nullptr_t.
+  */
   option_t() noexcept : _defined(false) { /* nop */ }
 
 
 
+  /// @brief Copy constructor.
   option_t(option_t const &opt) noexcept
   : _defined(opt._defined) { /* nop */ }
 
 
 
+  /// @brief Move constructor.
   option_t(option_t &&opt) noexcept
   : _defined(opt._defined)
   {
@@ -583,6 +669,7 @@ public:
 
 
 
+  /// @brief Copy-assignment operator.
   option_t &operator = (option_t const &opt) noexcept
   {
     return assign(opt);
@@ -590,6 +677,7 @@ public:
 
 
 
+  /// @brief Move-assignment operator.
   option_t &operator = (option_t &&opt) noexcept
   {
     return assign(std::forward<option_t>(opt));
@@ -597,6 +685,7 @@ public:
 
 
 
+  /// @brief Assign nothing to the option.
   option_t &operator = (none_t const &non) noexcept
   {
     return assign(non);
@@ -604,6 +693,7 @@ public:
 
 
 
+  /// @brief Assign nothing to the option.
   option_t &assign(none_t const &non) noexcept
   {
     _defined = false;
@@ -612,6 +702,7 @@ public:
 
 
 
+  /// @brief Copy assignment.
   option_t &assign(option_t const &opt) noexcept
   {
     _defined = opt._defined;
@@ -620,6 +711,7 @@ public:
 
 
 
+  /// @brief Move assignment.
   option_t &assign(option_t &&opt) noexcept
   {
     _defined = opt._defined;
@@ -629,6 +721,8 @@ public:
 
 
 
+  /// @brief Gets the value of the option if defined, otherwise throws an
+  /// std::runtime_error exception.
   std::nullptr_t get() const
   {
     if (is_empty()) {
@@ -639,6 +733,8 @@ public:
 
 
 
+  /// @brief Gets the value of the option if defined, otherwise throws an
+  /// std::runtime_error exception.
   std::nullptr_t operator * () const
   {
     if (is_empty()) {
@@ -649,6 +745,13 @@ public:
 
 
 
+  /**
+    @brief Recast option and its value ti an option of type U.
+
+    This attempts to construct a new object of type U by passing a const
+    reference or the value of this option to the new option's constructor.
+    The new option's value is constructed in place.
+  */
   template <typename U>
   operator option_t<U> () const
   {
@@ -661,6 +764,10 @@ public:
 
 
 
+  /**
+    @brief Apply the function f to the value of the option if and only if the
+    option has a defined value.
+  */
   template <typename F>
   void operator () (F &&f) const
   {
@@ -671,9 +778,28 @@ public:
 
 
 
+  /**
+    @brief Apply the function f to the value of the option if and only if the
+    option has a defined value and return a new option with the result of the
+    function.
+
+    The function f must return a value of type U (whatever type the function
+    returns), which will be stored as the value of an optional<U>. The function
+    must return something. If you need to be able to return nothing for
+    possibly unwanted values, use flat_map to return either some(U) or none.
+  */
   template <typename F>
   auto map(F &&f) const -> optional<decltype(f(nullptr))>;
 
+  /**
+    @brief Apply the function f to the value of the option if and only if the
+    option has a defined value and return a new option with the result of the
+    function.
+
+    Similar to map, except that function f must return an optional<U> instead
+    of just a U. As such, the function may choose to return either nothing or
+    something.
+  */
   template <typename F>
   auto flat_map(F &&f) const -> optional<typename decltype(f(nullptr))::value_type>;
 };
@@ -706,12 +832,10 @@ auto option_t<std::nullptr_t>::flat_map(F &&f) const -> optional<typename declty
 
 
 
-/*==============================================================================
-  some(T &&)
-
-    Creates a new option_t<T> and returns it, either copying or moving the value
-    provided.
-==============================================================================*/
+/**
+  @brief Creates a new option_t<T> and returns it, either copying or moving the
+  value provided.
+*/
 template <typename T>
 optional<T> some(T &&val)
 {
@@ -719,6 +843,11 @@ optional<T> some(T &&val)
 }
 
 
+/**
+  @brief Creates a new optional and constructs its value in-place using the
+  given arguments, forwarded to T's cnstructor. Just an alias around
+  optional<T>::make(...).
+*/
 template <typename T, typename... ARGS>
 optional<T> make_optional(ARGS&&... args)
 {
